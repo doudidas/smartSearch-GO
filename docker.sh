5e5146b2a92cd7c028272fae1b908fc70e79c4c3
Edouard Topin <ed.topin@gmail.com>
2019-03-25 10:28:45 +0100

    fix

--- docker.sh
+++ docker.sh
@@ -2,7 +2,7 @@
 # Git adn dep are mendatory to get go dependancies
  apk update && apk add --no-cache git dep 
 # Get dependancies with dep
-dep ensure - v
+dep ensure
 # Build a scratch version of the code
 # GOOS=linux GOARCH=amd64 go build -ldflags="-w -s" -o /go/bin/main
 go build
