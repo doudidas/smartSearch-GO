237e21f6b38fd38b747aecdc8338ff3e2ff59f66
Edouard Topin <ed.topin@gmail.com>
2019-08-21 19:00:42 +0200

    basic auth added

--- app.go
+++ app.go
@@ -9,33 +9,36 @@ func main() {
 	router := gin.Default()
 	router.Use(location.Default())
 	initDB()
+	admin := gin.Accounts{"admin": "VMware1!"}
 
-	router.GET("api/ping", func(c *gin.Context) {
+	router.GET("ping", func(c *gin.Context) {
 		c.String(200, "pong")
 	})
-	router.GET("api/healthcheck", func(c *gin.Context) {
-		err := pingMongo()
-		if err != nil {
-			c.AbortWithError(500, err)
+	adminGroup := router.Group("/api", gin.BasicAuth(admin))
+	{
+		adminGroup.GET("healthcheck", func(c *gin.Context) {
+			err := pingMongo()
+			if err != nil {
+				response := gin.H{"api": "true", "mongo": "false"}
+				c.JSONP(200, response)
+			}
+			response := gin.H{"api": "true", "mongo": "true"}
+			c.JSON(200, response)
+		})
+		userGroup := adminGroup.Group("user")
+		{
+			userGroup.GET("", getUsers)
+			userGroup.GET(":userID", getUserbyID)
+			userGroup.DELETE(":userID", deleteUserByID)
+			userGroup.PUT(":userID/email/:email", modifyUserEmail)
+			userGroup.PUT(":userID", modifyUserbyID)
+			userGroup.POST("", createUser)
 		}
-		response := gin.H{"api": "true", "mongo": "true"}
-		c.JSON(200, response)
-	})
-
-	userGroup := router.Group("api/user")
-	{
-		userGroup.GET("", getUsers)
-		userGroup.GET(":userID", getUserbyID)
-		userGroup.DELETE(":userID", deleteUserByID)
-		userGroup.PUT(":userID/email/:email", modifyUserEmail)
-		userGroup.PUT(":userID", modifyUserbyID)
-		userGroup.POST("", createUser)
+		topicGroup := adminGroup.Group("topic")
+		{
+			topicGroup.GET("", func(c *gin.Context) {})
+			topicGroup.GET("/:topicID", func(c *gin.Context) {})
+		}
 	}
-	topicGroup := router.Group("api/topic")
-	{
-		topicGroup.GET("", func(c *gin.Context) {})
-		topicGroup.GET("/:topicID", func(c *gin.Context) {})
-	}
-
 	router.Run(":80")
 }
